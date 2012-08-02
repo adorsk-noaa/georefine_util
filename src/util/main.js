2cc4e9442bc66173d72398471ebfb3b452d140e2
adorsk <adorsk>
2012-08-02 11:09:38 -0400

    fixed friendly number to account for sign of number

--- src/util/main.js
+++ src/util/main.js
@@ -17,6 +17,8 @@ function($){
         "friendlyNumber": function(number, decPlaces, use_long) {
             decPlaces = Math.pow(10,decPlaces);
             number = parseFloat(number);
+            var sign = (number < 0) ? -1 : 1;
+            var absNumber = Math.abs(number);
             var long_suffixes = ["", " thousand", " million", " billion", " trillion", " quadrillion", " quintillion"];
             var short_suffixes = " kMGTPE";
             var suffixes = use_long ? long_suffixes : short_suffixes;
@@ -24,11 +26,11 @@ function($){
                 var i = 0;
             for (i=suffixes.length-1; i>=-1; i--) {
                 size = Math.pow(10,(i+1)*3);
-                if (size <= number) {
+                if (size <= absNumber) {
                     break;
                 }
             }
-            number = Math.round(number*decPlaces/size)/decPlaces;
+            number = sign * Math.round(absNumber*decPlaces/size)/decPlaces;
             var suffix = (i == -2) ? suffixes[0] : suffixes[i+1];
             return number.toString() + suffix;
         }
